d526da53d332786931f26eff82757c442d98c4f1
Jie Huang <jiehuang.fi@gmail.com>
2024-07-01 13:57:12 +0300

    delete output folder

--- 0_prepare/01_run_1_chandler_fast.sh
+++ 0_prepare/01_run_1_chandler_fast.sh
@@ -6,5 +6,6 @@ ln -s ../m1_resample/128w-pos-1_sampled.xyz 128w-pos-1_sampled.xyz
 python3 1_chandler_fast.py < input_chandler_128w
 
 # Move output files to output directory
+rm -rf output
 mkdir -p output
 mv *.cube output/
