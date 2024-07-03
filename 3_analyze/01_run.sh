657a23850af89db0b8a9e1f744812478af025e4f
Jie Huang <jiehuang.fi@gmail.com>
2024-07-03 20:21:52 +0300

    add analyze

---
+++ 3_analyze/01_run.sh
@@ -0,0 +1,39 @@
+mkdir -p output
+for scenario in 1_case1 2_case2;
+do
+	echo "Processing ${scenario}..."
+	# Create symbolic links to the output files
+	for i in `seq 1 1 6`; 
+	do 
+		# The symbol * is used to match 1.dat and 1.0.dat 
+		ln -s ../${scenario}/output/128w_itp_wat_pair_hbacf_h_ihb_${i}*.dat c_${i}.dat; 
+	done
+
+	# Extract c(t_f) from files `c_${i}.dat`, where t_f = 0, 1, 2,... ps.
+	# ns = 1, dt = 0.04 ps, unit = 1 ps, rows = unit/(dt*ns)
+	rowNum=25
+	for i in `seq 1 1 6`; 
+	do 
+		awk -v rowNum=$rowNum "NR%rowNum==1{print}" c_${i}.dat > output/c_${i}_at_ref.dat; 
+	done
+
+	# Combine values of special ref. time (1, 2, 5, 10) for all layers.
+	cd output
+	rm -rf layers_c_at_ref_temp.dat
+	touch layers_c_at_ref_temp.dat 
+	for i in `seq 1 1 6`;
+	do  
+		awk 'NR==2 || NR==3 || NR==6 || NR==11{print}' c_${i}_at_ref.dat > ${i}_at_ref.dat
+		cat layers_c_at_ref_temp.dat ${i}_at_ref.dat > tmp
+		mv tmp layers_c_at_ref_temp.dat
+		rm ${i}_at_ref.dat
+	done
+	rm -rf c_*_at_ref.dat
+	cd ..
+
+	rm -rf c_*.dat
+	# Format the output file
+	./formatting.py output/layers_c_at_ref_temp.dat  output/${scenario}_layers_c_at_ref.dat
+	rm output/layers_c_at_ref_temp.dat
+done
+
---
+++ 3_analyze/formatting.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import numpy as np
+
+# Obtain command line arguments
+import sys
+if len(sys.argv) != 3:
+    print("Usage: python formatting.py <input_file> <output_file>")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+data = np.loadtxt(input_file)
+
+# Function to write the data block to a file
+def write2file(block, t, f):
+    t = int(t)
+    f.write("#t={} ps\n".format(t))
+    for i in range(block.shape[0]):
+        d = i + 1 # This is the layer number
+        f.write("{}        {}        {}\n".format(d, block[i, 1], block[i, 2])) 
+    f.write("\n\n")
+
+# Create a file to write the data
+f = open(output_file, 'w')
+
+timeSteps = np.array([1.0, 2.0, 5.0, 10.0])
+for t in timeSteps:
+    print("Time step: ", t)
+    mask = data[:, 0] == t
+    data_t = data[mask]
+    write2file(data_t, t, f)
+f.close()
+
