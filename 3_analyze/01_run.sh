439013d7952db048ef0abf3931a87ebc3e18509e
Jie Huang <jiehuang.fi@gmail.com>
2024-07-04 19:42:51 +0300

    add fit_reactive_flux.py

--- 3_analyze/01_run.sh
+++ 3_analyze/01_run.sh
@@ -1,5 +1,6 @@
 mkdir -p output
-for scenario in 1_case1 2_case2;
+#for scenario in 1_case1 2_case2;
+for scenario in 2_case2
 do
 	echo "Processing ${scenario}..."
 	# Create symbolic links to the output files
@@ -35,5 +36,18 @@ do
 	# Format the output file
 	./formatting.py output/layers_c_at_ref_temp.dat  output/${scenario}_layers_c_at_ref.dat
 	rm output/layers_c_at_ref_temp.dat
+
+
+	# Fit reactive flux constants for all layers
+	kkprime=output/${scenario}_kkprime.dat
+	rm -rf $kkprime
+	for i in {1..6}
+	do
+		filePath=../${scenario}/output
+		./fit_reactive_flux.py ${filePath}/128w_itp_wat_pair_hbacf_h_ihb_${i}*.dat \
+			${filePath}/128w_itp_wat_pair_hbacf_n_ihb_${i}*.dat \
+			${filePath}/128w_itp_wat_pair_hbacf_k_ihb_${i}*.dat >> $kkprime
+	done
+
 done
 
--- 3_analyze/fit_reactive_flux.py
+++ 3_analyze/fit_reactive_flux.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import sys
-
-# Calculate the coefficients k and k_prime in k=kc +k_prime n.
+import sys, datetime
 
 # Get the command line arguments using sys.argv
 if len(sys.argv) != 4:
@@ -20,38 +17,22 @@ k_file = sys.argv[3]
 #IF STUDY LONG-TIME RELAXATION
 start = 5
 end = 300 
+dt = 0.04
 
 # IF STUDY SHORT-TIME RELAXTION
-df_c = pd.read_csv(c_file, header=None)
-df_c.columns = ["time","c"]
-df_c.head()
-#print("shape of c:{}".format(df_c.shape))
+data_c = np.loadtxt(c_file)
+data_n = np.loadtxt(n_file)
+data_k = np.loadtxt(k_file)
 
-# load x2= n(t)
-df_n = pd.read_csv(n_file, header=None)
-df_n.columns = ["time","n"]
-df_n.head()
-#print("shape of n:{}".format(df_n.shape))
-
-df_k = pd.read_csv(k_file, header=None)
-df_k.columns = ["time","k"]
-df_k.head()
-
-c_index= df_c['time'].values
-c=df_c['c'].values
-c = c[start:end]
-c_index = c_index[start:end]
-
-n=df_n['n'].values
-n = n[start:end]
-#print("n.shape={}".format(n.shape))
-
-k=df_k['k'].values
-k = k[start:end]
-#print("shape of k:{}".format(df_k.shape))
+simTime = data_c[:,0] 
+timeMask = (simTime >= start*dt) & (simTime < end*dt)
+simTime = simTime[timeMask]
+c = data_c[timeMask,1]
+n = data_n[timeMask,1]
+k = data_k[timeMask,1]
+print('k', k)
 
 A = np.stack((c, n), 1)
-
 # If we write $c(t)$, $n(t)$ and $k(t)$ as vectors,and denote 
 # them as ${\bf c}$,${\bf n}$ and ${\bf k}$. The $ k$ and $k'$ 
 # can be determined from the matrix $ A = ({\bf c} \ {\bf n})$, 
@@ -59,15 +40,17 @@ A = np.stack((c, n), 1)
 # We can obtain $k$ and $k'$ by
 # $[k ,\ -k']^T= (A^T A)^{-1} A^T {\bf k}$.
 A_trans = A.T 
+print('A_trans', A_trans)
 B = np.matmul(A_trans, A)
-# Calculate the inverse of Matrix B
 B_inv = np.linalg.inv(B)
+print('B_inv', B_inv)
 b1 = np.matmul(A_trans,k)
+print('b1', b1)
 x = np.matmul(B_inv, b1)
+print(x)
+plt.plot(simTime, x[0]*c +x[1]*n, '--')
+plt.plot(simTime, k, 'r-')
+# 
+plt.savefig('kkprime_fit_' + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f") + '.png')
 
-plt.plot(c_index,x[0]*c +x[1]*n, '--')
-plt.plot(c_index,k, 'r-')
-plt.savefig('least_square_fit_reactive_flux_ADH.png')
-
-#print("(k, k') = {} {}".format(x[0], -x[1]))
 print("{} {}".format(x[0], -x[1]))
