494c8a08fff77d05dae85059aa12f918e9576947
Jie Huang <jiehuang.fi@gmail.com>
2024-07-07 13:48:04 +0300

    update m1_resample

--- m1_resample/01_run.sh
+++ m1_resample/01_run.sh
@@ -1,1 +1,13 @@
-python resample.py 128w-pos-1.xyz
+original_traj=128w-pos-1.xyz
+rm -rf output
+mkdir -p output
+start_frame=0
+sampled_traj=${original_traj%.xyz}-s${start_frame}.xyz
+python resample.py ${original_traj} ${start_frame} output/${sampled_traj}
+
+#for start_frame in 0 10000 20000 30000 40000 50000
+#do
+#    sampled_traj=${original_traj%.xyz}-s${start_frame}.xyz
+#    python resample.py ${original_traj} ${start_frame} output/${sampled_traj}
+#done
+
--- m1_resample/resample.py
+++ m1_resample/resample.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 import sys 
 
-if len(sys.argv) != 2:
-    print("Ooops. Usage:\n" + 'python ' + sys.argv[0] + ' 128w-pos-1.xyz')
+if len(sys.argv) != 4:
+    print("Ooops. Usage:\n" + 'python ' + sys.argv[0] + ' <original_traj>' + ' <start_frame>' + ' <sampled_traj>')
     exit()
 
-# Input file
+# Original trajectory file
 f1 = open(sys.argv[1],'r')
+# Start frame
+start_frame = int(sys.argv[2]) 
+# Output sampled file
+out_name = sys.argv[3]
+f2 = open(out_name, 'w')
+
 
 # Smaple every n frames
 ns = 80
 
-# Output file
-out_name = sys.argv[1].split('.xyz')[0] + '_sampled.xyz'
-f2 = open(out_name, 'w')
-
+# Sub-trajectory length in ps
+sub_length_in_ps = 15.0 
+sub_length_in_fs = sub_length_in_ps * 1000  
+dt = 0.5  # fs 
+numFrames = int(sub_length_in_fs / dt)  
+end_frame = start_frame + numFrames
 
 # File as a variable to get the atom numbers
 f3 = open(sys.argv[1], 'r')
@@ -22,6 +30,7 @@ num_atom = int(f3.readline())
 f3.close()
 print('Number of atoms:{}'.format(num_atom))
 
+totalFrames = 80000
 
 def read_one_frame(f1, num_atom):
     '''
@@ -44,18 +53,17 @@ def read_one_frame(f1, num_atom):
         print('End of file')
         return None, None
 
-# Read the first frame
-i, frame = read_one_frame(f1, num_atom)
-while i is not None and i < 80000:
-    if i % ns == 0:
-        for k, line in enumerate(frame):
-            if k == 1:
-                field1 = line.split(',')[0]
-                line = ' i =         ' + line[len(field1):]
-                new_i = str(i // ns)
-                line = line[0:len(field1)-len(new_i)] + new_i + line[len(field1):]
-            f2.write(line)
+for j in range(totalFrames):
     i, frame = read_one_frame(f1, num_atom)
+    if i is not None and i >= start_frame and i < end_frame:
+        if i % ns == 0:
+            for k, line in enumerate(frame):
+                if k == 1:
+                    field1 = line.split(',')[0]
+                    line = ' i =         ' + line[len(field1):]
+                    new_i = str((i-start_frame) // ns)
+                    line = line[0:len(field1)-len(new_i)] + new_i + line[len(field1):]
+                f2.write(line)
 
 f1.close()
 f2.close()
