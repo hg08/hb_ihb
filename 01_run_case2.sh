444f382903fa98b15c4bfa156657d2ab0b4c431d
Jie Huang <jiehuang.fi@gmail.com>
2024-07-18 17:11:13 +0300

    update case2 workflow, only the complete trajectory is used now

--- 01_run_case2.sh
+++ 01_run_case2.sh
@@ -5,57 +5,56 @@ else
     JSON_FILE=$1
 fi
 
+name=$(jq -r '.name' $JSON_FILE)
 system=$(jq -r '.system' $JSON_FILE)
+dt=$(jq -r '.dt' $JSON_FILE)
 sizeX=$(jq -r '.sizeX' $JSON_FILE)
 sizeY=$(jq -r '.sizeY' $JSON_FILE)
 sizeZ=$(jq -r '.sizeZ' $JSON_FILE)
 numAtom=$(jq -r '.numAtoms' $JSON_FILE)
-
-
-echo Processing system $system ...
-trajFile=$system.xyz 
-
+numFrame=$(jq -r '.numFrames' $JSON_FILE)
+frameStart=0
+frameEnd=$(($numFrame-1))
 
 # --- Step 2_case2
+echo Processing system $system ...
+trajFile=${name} 
 cd 2_case2 # (
 # Compile and clean up
 ./compile.sh
-for start_frame in 0 10000 20000 30000 40000 50000
+# a. Prepare input trajectory and parameter file
+surfTrajFile=surf_${system}.dat
+rm -rf $trajFile $surfTrajFile 
+ln -s ../m2_traj/$trajFile .
+ln -s ../0_prepare/output/$surfTrajFile .
+for d in {1..6}
 do 
-	# a. Prepare input trajectory and parameter file
-	subTraj=${system}_s${start_frame}
-	subTrajFile=${subTraj}.xyz
-	surfTrajFile=surf_${subTraj}.dat
-	rm -rf $subTrajFile $surfTrajFile 
-	ln -s ../m1_resample/output/$subTrajFile .
-	ln -s ../0_prepare/output/$surfTrajFile .
-	for d in {1..6}
-	do 
-		inputFile=input_${subTraj}_${d}
-		cp input_template $inputFile
-		sed -i "s/SIZEX/${sizeX}/g" $inputFile
-		sed -i "s/SIZEY/${sizeY}/g" $inputFile
-		sed -i "s/SIZEZ/${sizeZ}/g" $inputFile
-		sed -i "s/SYSTEM/${subTraj}/g" $inputFile
-		sed -i "s/SUBTRAJ/${subTraj}/g" $inputFile
-		sed -i "s/NUMATOM/${numAtom}/g" $inputFile
-		sed -i "s/SURFTRAJFILE/${surfTrajFile}/g" $inputFile
-		sed -i "s/THICKNESS/${d}/g" $inputFile
-	done
-
-	# b. Run ihb 
-	for d in {1..6}
-	do 
-		inputFile=input_${subTraj}_${d}
-		./main_interface_c_format2 < $inputFile
-		./main_interface_k_format2 < $inputFile
-		./main_interface_n_format2 < $inputFile
-	done
-
-	# c. Clean up
-	mkdir -p output
-	mv ${subTraj}_* output/
-
+	inputFile=input_${system}_${d}
+	cp input_template $inputFile
+	sed -i "s/SIZEX/${sizeX}/g" $inputFile
+	sed -i "s/SIZEY/${sizeY}/g" $inputFile
+	sed -i "s/SIZEZ/${sizeZ}/g" $inputFile
+	sed -i "s/DTINPS/${dt}/g" $inputFile
+	sed -i "s/SYSTEM/${system}/g" $inputFile
+	sed -i "s/FRAMESTART/${frameStart}/g" $inputFile
+	sed -i "s/FRAMEEND/${frameEnd}/g" $inputFile
+	sed -i "s/NUMATOM/${numAtom}/g" $inputFile
+	sed -i "s/SURFTRAJFILE/${surfTrajFile}/g" $inputFile
+	sed -i "s/THICKNESS/${d}/g" $inputFile
 done
+
+# b. Run ihb 
+for d in {1..6}
+do 
+	inputFile=input_${system}_${d}
+	./main_interface_c_format2 < $inputFile
+	./main_interface_k_format2 < $inputFile
+	./main_interface_n_format2 < $inputFile
+done
+
+# c. Clean up
+mkdir -p output
+mv ${system}_* output/
+
 cd .. # ) 
 # --- End step 2_case2
