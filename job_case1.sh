fd0587e998084449385b8a744c9df30d289f812b
Jie Huang <146966593+huangchieh@users.noreply.github.com>
2024-07-19 15:24:44 +0300

    add submmit scripts (#48)
    Co-authored-by: Jie Huang <jiehuang.fi@gmail.com>

---
+++ job_case1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=case1                  # Job name
+#SBATCH --output=%x_id_%j.out                # Output file based on job name and job ID
+#SBATCH --error=%x_id_%j.err                 # Error file based on job name and job ID
+#SBATCH --time=INFINITE                   # Time limit
+#SBATCH --partition=debug                 # Partition name
+#SBATCH --ntasks=1                        # Number of tasks
+#SBATCH --cpus-per-task=1                 # Number of CPU cores per task
+#SBATCH --mem-per-cpu=4G                  # Memory per CPU core
+
+echo "Running job with name ${SLURM_JOB_NAME} and ID ${SLURM_JOB_ID}"
+start_time=$(date +%s)
+echo "Job started at $(date)"
+
+# --- Your code here ---
+./01_run_case1.sh
+# ----------------------
+
+end_time=$(date +%s)
+echo "Job ended at $(date)"
+# Calculate and log the total time used in hours
+total_time=$((end_time - start_time))
+total_hours=$(echo "scale=2; $total_time / 3600" | bc)
+echo "Total time used: $total_hours hours"
---
+++ job_prepare.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=prepare                  # Job name
+#SBATCH --output=%x_id_%j.out                # Output file based on job name and job ID
+#SBATCH --error=%x_id_%j.err                 # Error file based on job name and job ID
+#SBATCH --time=INFINITE                   # Time limit
+#SBATCH --partition=debug                 # Partition name
+#SBATCH --ntasks=1                        # Number of tasks
+#SBATCH --cpus-per-task=1                 # Number of CPU cores per task
+#SBATCH --mem=1G                          # Memory per CPU core
+
+echo "Running job with name ${SLURM_JOB_NAME} and ID ${SLURM_JOB_ID}"
+start_time=$(date +%s)
+echo "Job started at $(date)"
+
+# --- Your code here ---
+./01_run_prepare.sh
+# ----------------------
+
+end_time=$(date +%s)
+echo "Job ended at $(date)"
+# Calculate and log the total time used in hours
+total_time=$((end_time - start_time))
+total_hours=$(echo "scale=2; $total_time / 3600" | bc)
+echo "Total time used: $total_hours hours"
