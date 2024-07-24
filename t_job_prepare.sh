a6a2a5c3b6b6ffc6115c03ccb48a3c86deb920ba
Jie Huang <jie.huang@aalto.fi>
2024-07-24 11:09:15 +0300

    add t_job_prepare.sh

---
+++ t_job_prepare.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=prepare                  # Job name
+#SBATCH --output=%x_id_%j.out                # Output file based on job name and job ID
+#SBATCH --error=%x_id_%j.err                 # Error file based on job name and job ID
+#SBATCH --time=01-00:00:00                   # Time limit
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40               # Number of tasks per node
+#SBATCH --mem=20G                          # Memory per CPU core
+
+# Automatically find the conda base path
+# Initialize conda in the shell
+#CONDA_BASE=$(conda info --base)
+#source "$CONDA_BASE/etc/profile.d/conda.sh"
+#conda activate ihb
+module load mamba gcc
+source activate ihb
+
+echo "Running job with name ${SLURM_JOB_NAME} and ID ${SLURM_JOB_ID}"
+start_time=$(date +%s)
+echo "Job started at $(date)"
+
+# --- Your code here ---
+./01_run_prepare.sh
+# ----------------------
+
+end_time=$(date +%s)
+echo "Job ended at $(date)"
+# Calculate and log the total time used in hours
+total_time=$((end_time - start_time))
+total_hours=$(echo "scale=2; $total_time / 3600" | bc)
+echo "Total time used: $total_hours hours"
