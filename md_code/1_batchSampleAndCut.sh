3b650b6aa55ec9e8c7f10a3c6125024cb43561b7
Jie Huang <jie.huang@aalto.fi>
2024-08-04 08:31:11 +0300

    update batch sample and cut

--- md_code/1_batchSampleAndCut.sh
+++ md_code/1_batchSampleAndCut.sh
@@ -2,9 +2,7 @@ for numWater in 125 216 343 512 729 1000
 do
 	echo "Processing $numWater"
 	# Get current path
-	basePath=$(pwd)
-	path="MB-pol/125h2o/"
-	cd $path
-	$basePath/sampleAndCut.py dump.xyz ${numWater}h2o-240ps-mbx-pol.xyz
-	cd $basePath
+	inputFile="MB-pol/{}h2o/dump.xyz".format(numWater)
+	outputFile="{}h2o-240ps-mbx-pol.xyz".format(numWater)
+	./sampleAndCut.py $inputFile $outputFile
 done
