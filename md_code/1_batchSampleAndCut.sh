5898729227b186ad58f03eb7a2f4383b0caae7b9
Jie Huang <jie.huang@aalto.fi>
2024-08-04 08:42:28 +0300

    fix grammar

--- md_code/1_batchSampleAndCut.sh
+++ md_code/1_batchSampleAndCut.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 for numWater in 125 216 343 512 729 1000
 do
-	echo "Processing $numWater"
-	# Get current path
-	inputFile="MB-pol/{}h2o/dump.xyz".format(numWater)
-	outputFile="{}h2o-240ps-mbx-pol.xyz".format(numWater)
-	./sampleAndCut.py $inputFile $outputFile
+    echo "Processing $numWater"
+    # Get current path
+    inputFile="MB-pol/${numWater}h2o/dump.xyz"
+    outputFile="${numWater}h2o-240ps-mbx-pol.xyz"
+    ./sampleAndCut.py $inputFile $outputFile
 done
