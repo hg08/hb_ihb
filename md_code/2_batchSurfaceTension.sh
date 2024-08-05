124681b35643a29d03ca553ec89a0dbd089eca14
Jie Huang <jie.huang@aalto.fi>
2024-08-05 10:31:21 +0300

    add batch surface tension calculations

---
+++ md_code/2_batchSurfaceTension.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#simType="TIP4P2005"
+simType="MB-pol"
+for numWater in 125 216 343 512 729 1000
+do
+    echo "Processing $numWater"
+    if [ $simType == "MB-pol" ]; then
+    	inputFile="MB-pol/${numWater}h2o/log.lammps"
+    else 
+    	inputFile="TIP4P2005/${numWater}w/log.lammps"
+    fi
+
+    python surfaceTension.py $inputFile # Output to output folder
+done
