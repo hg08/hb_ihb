64944fe1a4058dc0843f77c870a29b62fec99988
Jie Huang (黄杰) <146966593+huangchieh@users.noreply.github.com>
2024-08-05 09:19:36 +0300

    TIP4P production trajectories praparations (#96)
    * add surface tensions for TIP4P2005
    
    * determine simulation type from lammps log file
    
    * compatable with TIP4P2005
    
    ---------
    
    Co-authored-by: Jie Huang <jiehuang.fi@gmail.com>

--- md_code/1_batchSampleAndCut.sh
+++ md_code/1_batchSampleAndCut.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+simType="TIP4P2005"
+#simType="MB-pol"
 for numWater in 125 216 343 512 729 1000
 do
     echo "Processing $numWater"
-    # Get current path
-    inputFile="MB-pol/${numWater}h2o/dump.xyz"
-    outputFile="${numWater}h2o-240ps-mbx-pol.xyz"
+    if [ $simType == "MB-pol" ]; then
+    	inputFile="MB-pol/${numWater}h2o/dump.xyz"
+    	outputFile="${numWater}h2o-240ps-mbx-pol.xyz"
+    else 
+    	inputFile="TIP4P2005/${numWater}w/dump.xyz"
+    	outputFile="${numWater}w-240ps-tip4p.xyz"
+    fi
+
     ./sampleAndCut.py $inputFile $outputFile
 done
