7c83af9a6724bcdcaf3151f57cb1fa64ef4f7e1f
Jie Huang (黄杰) <146966593+huangchieh@users.noreply.github.com>
2024-08-06 18:43:02 +0300

    update surface tension figures (#106)
    Co-authored-by: Jie Huang <jiehuang.fi@gmail.com>

--- md_code/plotSurfaceTensions.py
+++ md_code/plotSurfaceTensions.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 #ST_mean_std_343_MB-pol.txt
 simType = 'MB-pol' if len(sys.argv) < 2 else sys.argv[1]
+label = 'TIP4P/2005' if simType == 'TIP4P2005' else simType
 numMolecules = np.array([125, 216, 343, 512, 729, 1000])
 meanValues = np.zeros(numMolecules.shape)
 stdValues = np.zeros(numMolecules.shape)
@@ -14,16 +15,19 @@ for idx, num in enumerate(numMolecules):
     data = np.loadtxt(filename)
     meanValues[idx], stdValues[idx], stdErrors[idx] = data[0], data[1], data[2]
 
-plt.figure(figsize=(6, 2.5))
-plt.axhline(y=71.73, linestyle='--', label='71.73 mN/m')
-plt.errorbar(numMolecules, meanValues, yerr=stdErrors, fmt='o', color='black', capsize=5, ecolor='black', label=simType)
+cube_roots = np.cbrt(numMolecules)
+scale = 5.0
+offset = -0.2
+plt.figure(figsize=(1*scale, 1*scale))
+plt.axhline(y=71.73, linestyle='--', color='grey', linewidth=0.8, label='Experiment')
+plt.errorbar(cube_roots, meanValues, yerr=stdErrors, fmt='o', color='black', capsize=5, ecolor='black', label=label)
+plt.xticks(cube_roots, labels=numMolecules)  # Label ticks with the actual cubic numbers
 plt.xlabel('Number of water molecules')
-plt.ylabel('Surface tension (mN/m)')
-plt.xticks(numMolecules)
-plt.tick_params(direction="in", axis='both', top=True)
+plt.ylabel('Surface tension $\gamma$ (mN/m)')
+plt.tick_params(direction="in", axis='both', which='both', top=True, right=True)
 plt.ylim(40, 100)
 plt.legend(frameon=False, ncol=2, loc='upper left')
-plt.tight_layout()
-plt.savefig('ST_{}.png'.format(simType))
+#plt.tight_layout()
+plt.savefig('ST_{}.png'.format(simType), dpi=300)
 plt.savefig('ST_{}.pdf'.format(simType))
-plt.show()
+#plt.show()
